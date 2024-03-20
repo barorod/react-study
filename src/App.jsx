b72ffd4c8ebebcbb699f9e1c86693c6d0cfde1df
Barorod <128448506+barorod@users.noreply.github.com>
2024-03-20 13:40:52 +0800

    feat: Add Pizza Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import './App.css';
+import './Pizza.css';
+import pizzaData from './data';
+import PizzaHeader from './PizzaHeader';
+import PizzaFooter from './PizzaFooter';
+import Pizza from './Pizza';
+import { useEffect } from 'react';
 
 const App = () => {
-  return <></>;
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+  useEffect(() => {
+    document.title = 'Fast React Pizza Co.';
+  }, []);
+
+  return (
+    <div className='container'>
+      <PizzaHeader />
+      <main className='menu'>
+        <h2>Our Menu</h2>
+        {numPizzas > 0 ? (
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        ) : null}
+      </main>
+      <PizzaFooter />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/PizzaHeader.jsx
@@ -0,0 +1,7 @@
+const PizzaHeader = () => (
+  <header>
+    <h1 className='header'>Fast React Pizza Co.</h1>
+  </header>
+);
+
+export default PizzaHeader;
