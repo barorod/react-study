325245e888e1b9d53074782249083031a0598798
Barorod <128448506+barorod@users.noreply.github.com>
2024-03-23 03:39:37 +0800

    refactor: Remove counter component. Add reset button

--- src/DateToday.jsx
+++ src/DateToday.jsx
@@ -3,39 +3,42 @@ import Counter from './Counter';
 
 const DateToday = () => {
   const [counts, setCounts] = useState({ count: 0, step: 1 });
+  const handleReset = () => setCounts((c) => ({ ...c, count: 0, step: 1 }));
 
-  const handlePlus = (val) => {
-    if (val === 'step') {
-      setCounts((c) => ({ ...c, [val]: c[val] + 1 }));
-    } else {
-      setCounts((c) => ({ ...c, count: c.count + counts.step }));
-    }
-  };
-
-  const handleMinus = (val) => {
-    if (val === 'step') {
-      setCounts((c) => ({ ...c, [val]: c[val] - 1 }));
-    } else {
-      setCounts((c) => ({ ...c, count: c.count - counts.step }));
-    }
-  };
   const date = new Date();
   date.setDate(date.getDate() + counts.count);
   return (
     <div>
-      <Counter
-        text={'Step'}
-        count={counts.step}
-        plus={() => handlePlus('step')}
-        minus={() => handleMinus('step')}
+      <input
+        type='range'
+        min='0'
+        max='10'
+        value={counts.step}
+        onChange={(e) => setCounts({ ...counts, step: Number(e.target.value) })}
       />
-      <Counter
-        text={'Count'}
-        count={counts.count}
-        plus={() => handlePlus('count')}
-        minus={() => handleMinus('count')}
-        step={counts.step}
-      />
+      <span>Step: {counts.step}</span>
+      <div>
+        <button
+          onClick={() =>
+            setCounts((c) => ({ ...c, count: counts.count - counts.step }))
+          }>
+          -
+        </button>
+        <input
+          type='text'
+          value={counts.count}
+          onChange={(e) =>
+            setCounts((c) => ({ ...c, count: Number(e.target.value) }))
+          }
+        />
+        <button
+          onClick={() =>
+            setCounts((c) => ({ ...c, count: counts.count + counts.step }))
+          }>
+          +
+        </button>
+      </div>
+
       <p>
         {counts.count === 0
           ? 'Today is '
@@ -44,6 +47,11 @@ const DateToday = () => {
           : `${Math.abs(counts.count)} days ago was `}
         {date.toDateString()}
       </p>
+      {counts.count !== 0 || counts.step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>reset</button>
+        </div>
+      ) : null}
     </div>
   );
 };
