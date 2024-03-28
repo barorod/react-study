f574de9ff53027f43979fee07a8b7dc7a171dbea
Barorod <128448506+barorod@users.noreply.github.com>
2024-03-29 00:09:06 +0800

    fix: Title style on click

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const Item = ({ num, title, text }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleToggle = () => setIsOpen((isOpen) => !isOpen);
+  const handleToggle = () => setIsOpen((currIsOpen) => !currIsOpen);
   return (
     <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
       <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
